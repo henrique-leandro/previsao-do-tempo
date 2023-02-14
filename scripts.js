ee7a6c3a8811c0b25bdd143707954cb8da001737
henrique-leandro <henrique.leandro.sales@gmail.com>
2023-02-14 17:21:49 -0300

    First commit

---
+++ scripts.js
@@ -0,0 +1,36 @@
+// variaveis
+
+let chave = "e61213d8c8a962343c19da0330fdc645"
+
+function colocaNaTela(dados) {
+    console.log(dados)
+
+    document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name
+    document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C"
+    document.querySelector(".descricao").innerHTML = dados.weather[0].description
+    document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png"
+    document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity
+}
+
+async function buscarCidade(cidade) {
+    
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + 
+    cidade +
+    "&appid=" +
+    chave +
+    "&lang=pt_br" +
+    "&units=metric")
+    .then(resposta => resposta.json())
+
+
+    colocaNaTela(dados)
+
+}
+
+function cliqueiNoBotao() {
+   let cidade = document.querySelector(".input-cidade").value
+
+   buscarCidade(cidade)
+}
+
+cliqueiNoBotao();
